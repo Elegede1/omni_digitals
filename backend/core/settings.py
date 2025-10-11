d4e3d6a814e6ec2a3f9853159e4da2a97dd10e55
Elegede1 <jekuthielnnamdi@gmail.com>
2025-10-11 02:11:17 +0100

    feat: Add contents of requirements.txt for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,10 +132,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': os.environ.get('DB_NAME', 'omni-digitals-db'),
+        'CLIENT': {
+            'host': os.environ.get('DB_HOST', 'mongodb://localhost:27017/'),
+        }
+    }
 }
 
 
