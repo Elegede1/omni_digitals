623d88a1c78b13ee780f7e9d8b0e4fc6c05130fa
Elegede1 <jekuthielnnamdi@gmail.com>
2025-10-29 12:24:51 +0100

    fix the deprecation warning and provide the foundation for you to style the login pages

--- backend/accounts/adapter.py
+++ backend/accounts/adapter.py
@@ -1,5 +1,5 @@
+from allauth.core.exceptions import ImmediateHttpResponse
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from allauth.exceptions import ImmediateHttpResponse
 from django.shortcuts import redirect
 from django.conf import settings
 from django.contrib.auth import get_user_model
